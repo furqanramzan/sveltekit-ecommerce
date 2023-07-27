ac7d4678333e5f210385f1876eed35e2c3810c45
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-07-27 15:02:03 +0500

    feat: add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+  ],
+  plugins: [require('flowbite/plugin')],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.orange,
+      },
+    },
   },
-  plugins: [],
 };
