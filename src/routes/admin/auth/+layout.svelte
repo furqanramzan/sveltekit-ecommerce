43e7811179636142851aacc0d646cf430cdc0bd1
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-08-01 23:26:00 +0500

    fix: fix admin layout margin issue

--- src/routes/admin/auth/+layout.svelte
+++ src/routes/admin/auth/+layout.svelte
@@ -37,8 +37,8 @@
 
   <Sidebar {links} />
 
-  <div class="flex min-h-screen flex-col gap-y-5 bg-gray-100 px-6 pb-6 dark:bg-gray-900 md:ml-64">
-    <section class="mt-24 rounded-lg bg-white p-8 shadow-lg dark:bg-gray-800">
+  <div class="flex min-h-screen flex-col gap-y-5 bg-gray-100 p-6 dark:bg-gray-900 md:ml-64">
+    <section class="mt-16 rounded-lg bg-white p-8 shadow-lg dark:bg-gray-800">
       <slot />
     </section>
 
--- src/routes/admin/auth/components/Sidebar.svelte
+++ src/routes/admin/auth/components/Sidebar.svelte
@@ -8,10 +8,10 @@
 </script>
 
 <aside
-  class="fixed left-0 top-0 z-40 mt-24 h-screen w-64 -translate-x-full border-r border-gray-200 bg-white transition-transform dark:border-gray-700 dark:bg-gray-800 md:translate-x-0"
+  class="fixed left-0 top-0 z-40 mt-16 h-screen w-64 -translate-x-full border-r border-gray-200 bg-white transition-transform dark:border-gray-700 dark:bg-gray-800 md:translate-x-0 py-6"
   aria-label="Sidebar"
 >
-  <div class="h-full overflow-y-auto bg-white px-3 pb-4 dark:bg-gray-800">
+  <div class="h-full overflow-y-auto bg-white px-3 dark:bg-gray-800">
     <ul class="space-y-2 font-medium">
       {#each links as link}
         <Link {link} />
